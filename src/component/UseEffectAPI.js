0a3c6fdbe2721e4104cad29d7d2f60e0ad556983
Lalit Dogra <lalit@rvsmedia.com>
2023-02-09 10:29:04 +0530

    loading done in about page

--- src/component/UseEffectAPI.js
+++ src/component/UseEffectAPI.js
@@ -1,43 +1,29 @@
 import React, { useEffect, useState } from "react";
+import Loading from "../gitHub/Loading";
+import GitHubUsers from "../gitHub/GitHubUsers";
 
 const UseEffectAPI = () =>{
     
     const [users,setUsers] = useState([]);
+    const [loading,setLoading] = useState(true);
     const getUsers = async() =>{
-       const response = await fetch("https://api.github.com/users");
-       setUsers(await response.json());
+      try{
+         setLoading(false);
+         const response = await fetch("https://api.github.com/users");
+         setUsers(await response.json());
+      }catch(error){
+        console.log(error);
+      }
     }
     useEffect(()=>{
        getUsers();
     },[])
+    if(loading){
+      return <Loading/>
+    }
     return( 
         <>
-        <div className="container mt-5 mb-5">
-           <h2 className="text-center">List of GitHub Users</h2>
-            <div className="row">
-               {
-                users.map((curElem) =>{
-                   return (
-                    <div className="col-10 col-md-4 mt-5" key={curElem.id}>
-                      <div className="card mb-3">
-                         <div className="row g-0">
-                            <div className="col-md-4 avatar_card">
-                              <img src={curElem.avatar_url}  alt="..." width="160"/>
-                            </div>
-                            <div className="col-md-8">
-                              <div className="card-body">
-                                <h5 className="card-title">{curElem.login}</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                              </div>
-                            </div>
-                          </div>
-                       </div>
-                    </div>
-                   )
-                })
-               }
-            </div>
-        </div>
+        <GitHubUsers users = { users }/>
         </>
   )
 }
